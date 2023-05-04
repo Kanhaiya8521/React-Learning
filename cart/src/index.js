9f9d7d235f621683b1816183f526ac0af275e37e
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-05-05 01:20:51 +0530

    added firebase successfully

--- cart/src/index.js
+++ cart/src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPaO7N0S8_DS0cx3cAzDPG0QXGVkXPNO8",
+  authDomain: "cart-79781.firebaseapp.com",
+  projectId: "cart-79781",
+  storageBucket: "cart-79781.appspot.com",
+  messagingSenderId: "757307953618",
+  appId: "1:757307953618:web:386b456cfe6408ae69f80b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
