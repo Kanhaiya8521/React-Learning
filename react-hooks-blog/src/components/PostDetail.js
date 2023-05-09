84f504710adebbcc0d46281d0a3a364ed9ecc6d4
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-05-09 15:58:10 +0530

    added post detail

--- react-hooks-blog/src/components/PostDetail.js
+++ react-hooks-blog/src/components/PostDetail.js
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {db} from '../firebase';
+import {doc, getDoc} from 'firebase/firestore'
+
 function PostDeatil() {
-    return <div className="">PostDeatil</div>
+
+    const [post, setPost] = useState({});
+    const {postId} = useParams();
+
+    useEffect(() => {
+        const getAllData = async () => {
+            const docRef = doc(db, "posts", postId);
+            const docSnap = await getDoc(docRef);
+
+            setPost(docSnap.data());
+          }
+          getAllData();
+    }, [])
+
+    return(
+        <div className="post-detail">
+            <h1>{post.title}</h1>
+            <h3>{post.subTitle}</h3>
+            <p>{post.content}</p>
+
+        </div>
+    ); 
 }
 
 export default PostDeatil;
